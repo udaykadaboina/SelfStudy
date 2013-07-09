d9a6f771b9cb0b06fa1adf534b51a22ab5d01841
Uday <uday88k@gmail.com>
2013-07-09 14:28:33 -0600

    added inflections

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SelfStudy::Application.config.secret_token = 'a01e7b4e1d1146c9ef51b21643be7e591375d6ea8897a77027f269ea44e7c1ff195aacf9c80d14119102a274cb4f0cfae66cb25e14aa7c021c506063d8372230'
